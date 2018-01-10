a1afcbdba131da2616df455bb177c49f646d27fa
James <1511877674@qq.com>
2018-01-10 12:37:59 +0800

    增加终端交互

--- fileConfig.js
+++ fileConfig.js
@@ -1,42 +1,100 @@
 const pageData = require('./pageData.json');
 const fs = require('fs');
 const path = require('path');
+const process = require('process');
 
-if( pageData.name ){
-    // 在配置文件父级的同级创建pageData.name文件
-    const basePath = path.resolve(__dirname, `../${pageData.name}`);
-    if( fs.existsSync(basePath) === false ){
-        fs.mkdir( basePath );
-    }
-    // 创建pageInfo下的配置内容
-    const pageInfo = pageData.pageInfo;
-    if( pageInfo && pageInfo.forEach ){
-        // 重新改造配置文件
-        pageInfo.forEach((item) => {
-            item.filePath =  `${basePath}/${item.name}`;
-            if( item.child ){
-                pageInfo.push({
-                    'name': item.child.name,
-                    'type': item.child.type,
-                    'content': item.child.content,
-                    'filePath': `${basePath}/${item.name}/${item.child.name}`
-                });
+const userstdin = {
+    confirm: false,
+    projectName: false
+};
+process.stdin.setEncoding('utf8');
+process.stdout.write(`请确定是否创建普通项目：(y/n)`);
+process.stdin.on('readable', () => {
+    const stdinRead = process.stdin.read();
+    if(typeof stdinRead === 'string'){
+        const chunk = stdinRead.replace(/(\r\n)|(\n)/g,'');
+        if( !userstdin.confirm ){
+            if(chunk === 'y'){
+                process.stdout.write(`请输入项目名称（默认为fePage，格式为 name:xxx）：`);
+                userstdin.confirm = true;
+            } else if (chunk === 'n') {
+                process.stdin.write(`\n`); 
+                process.stdin.end();
+            } else {
+                process.stdout.write(`请确定是否创建普通项目：(y/n)`);
+                userstdin.confirm = false;
             }
-        });
+            return;
+        };
+        
+        if( !userstdin.projectName ){
+            if( chunk.indexOf('name:') != -1 ){
+                new makeDirectory( `${chunk.split('name:')[1]}` );
+            } else if( chunk === '' ) {
+                new makeDirectory();
+            } else {
+                process.stdout.write(`请输入项目名称（默认为fePage，格式为 name:xxx）：`);
+                userstdin.projectName = false;
+            }
+        }
+    }
+});
+
+class makeDirectory{
+    constructor( dirName ){
+        this.dirName  = dirName || pageData.name;
+        this.basePath = path.resolve(__dirname, `../${this.dirName}`);
+        this.pageInfo = pageData.pageInfo;
+        this.makeFile();
+    };
+
+    makeFile(){
+        if( pageData.name ){
+            if( fs.existsSync( this.basePath ) === false ){
+                fs.mkdirSync( this.basePath );
+                this.assembleData();
+            } else {
+                process.stdin.write(`*=========*\n该文件名已存在\n*=========*\n`); 
+                process.stdin.end();
+            }
+        }
+    };
+    assembleData(){
+        if( this.pageInfo && this.pageInfo.forEach ){
+            // 重新改造配置文件
+            this.pageInfo.forEach((item) => {
+                item.filePath =  `${this.basePath}/${item.name}`;
+                if( item.child ){
+                    this.pageInfo.push({
+                        'name': item.child.name,
+                        'type': item.child.type,
+                        'content': item.child.content,
+                        'filePath': `${this.basePath}/${item.name}/${item.child.name}`
+                    });
+                }
+            });
+            this.mkEachDir();
+        };
+    };
+    mkEachDir(){
         // 创建文件
-        pageInfo.forEach((item) => {
+        this.pageInfo.forEach((item) => {
             switch( item.type ){
                 case 'dir': 
                     fs.mkdirSync( item.filePath );
                     break;
                 case 'file':
-                    let fileContent = item.content || '';
+                    const fileContent = item.content || '';
                     fs.writeFileSync( item.filePath, fileContent )
                     break;
                 default:
                     break;
             }
-        });
-   
-    }
+        }); 
+        this.endDirectory();
+    };
+    endDirectory(){
+        process.stdin.write(`*=========*\n一个基本的前端结构（名字为：${this.dirName}）创建成功\n*=========*\n`);
+        process.stdin.end();
+    };
 }
